02878775ad1227328bab53001eb79a08bafec760
maodazheng <maodazheng@163.com>
2015-09-22 11:23:05 +0800

    iOS change

--- config.sh
+++ config.sh
@@ -11,5 +11,5 @@ export DIOS_COCOS_PATH=${DIOS_PROJECT_PATH}/src/oslibs/cocos/cocos-src
 # -- DIOS ±‰¡ø
 # -------------------------------
 export DIOS_INSTALL=${DIOS_PROJECT_PATH}/install
-export DIOS_TOOLS=${DIOS_PATH}/build/tools/ios
+export DIOS_TOOLS=${DIOS_PATH}/build/tools
 export DIOS_CMAKE=${DIOS_PATH}/build/cmake
